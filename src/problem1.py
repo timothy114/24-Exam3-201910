6025900ba109d8b442cd08d7c5a6fa6d6d115f03
timothy114 <liwj@rose-hulman.edu>
2018-10-25 21:11:03 -0400

    Timothy Li

--- src/problem1.py
+++ src/problem1.py
@@ -2,9 +2,9 @@
 Exam 3, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  October 2018.
+         their colleagues and Timothy Li.  October 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -86,9 +86,16 @@ def problem1(numbers):
       :type numbers:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+
+    sum = 0
+    start = int(len(numbers)/3)
+    for i in range(start):
+        sum += numbers[start + i]
+    return sum
+
 
 
 ###############################################################################
